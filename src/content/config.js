4ba3315b5971ff59e5f55f55ef4f67188df7daa0
Felix Corke <felix@blackspike.com>
2025-05-21 16:36:44 +0300

    modernized collections

--- src/content/config.js
+++ src/content/config.js
@@ -1,11 +1,28 @@
 import { z, defineCollection } from 'astro:content'
-export const collections = {
-  slides: defineCollection({
-    schema: z.object({
-      title: z.string(),
-      image: z.string().optional(),
-      description: z.string().optional(),
-      order: z.number(),
-    }),
+import { glob, file } from 'astro/loaders'
+
+const slides = defineCollection({
+  loader: glob({ pattern: "**/*.mdx", base: "./src/content/slides" }),
+  schema: z.object({
+    title: z.string(),
+    image: z.string().optional(),
+    description: z.string().optional(),
+    order: z.number(),
   }),
-}
+})
+
+const pages = defineCollection({
+  loader: glob({ pattern: "**/*.mdx", base: "./src/content/pages" }),
+  schema: z.object({
+    title: z.string(),
+    image: z.string().optional(),
+    description: z.string().optional(),
+    order: z.number(),
+  }),
+})
+
+const meta = defineCollection({
+  loader: file("src/content/meta/settings.json"),
+})
+
+export const collections = { slides, pages, meta }
